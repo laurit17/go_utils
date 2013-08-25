be61d7b28668341df309bfd6b50287daf57cd644
Lauri Takacsi-Nagy <ltakacsi@gmail.com>
2013-08-24 23:10:47 -0700

    ready

--- string_utils/palindrome_test.go
+++ string_utils/palindrome_test.go
@@ -5,13 +5,18 @@ import "testing"
 func TestPalindrome(t *testing.T) {
 	const in, out = "", true
 	var inOutPairs = map[string]bool{
-		"":        true,
-		"a":       true,
-		"aa":      true,
-		"ab":      false,
-		"aba":     true,
-		"Aa":      false,
-		"abcdcba": true,
+		"":             true,
+		"a":            true,
+		"aa":           true,
+		"ab":           false,
+		"aba":          true,
+		"Aa":           false,
+		"abcdcba":      true,
+		"98":           false,
+		"89":           false,
+		"88":           true,
+		"87":           false,
+		"adblaskjdasd": false,
 	}
 	for in, out := range inOutPairs {
 		if x := IsPalindrome(in); x != out {
