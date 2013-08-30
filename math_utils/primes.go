d56eb0a40b44bb72b31a8a7c7086a1eed45f7fca
Lauri Takacsi-Nagy <ltakacsi@gmail.com>
2013-08-30 00:32:06 -0700

    created some more prime utilities

--- math_utils/primes.go
+++ math_utils/primes.go
@@ -14,6 +14,24 @@ func SieveOfEratosthenes() func() int {
 	}
 }
 
+func PrimesUpTo(n int) []bool {
+	sieve := make([]bool, n+1)
+
+	for i := 2; i < len(sieve); i++ {
+		sieve[i] = true
+	}
+
+	for i := 2; i*i < len(sieve); i++ {
+		if sieve[i] {
+			for j := 2; i*j < len(sieve); j++ {
+				sieve[i*j] = false
+			}
+		}
+	}
+
+	return sieve
+}
+
 func NthPrime(n int) int {
 	iter := SieveOfEratosthenes()
 	for i := 0; i < n-1; i++ {
